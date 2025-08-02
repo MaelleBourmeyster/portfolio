4c4810d0c8e416f3ee22cfcd59e2c519a9bb0eca
MaelleBourmeyster <maelle.bourmeyster@gmail.com>
2025-08-02 16:36:31 +0200

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,56 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Portfolio from '../App.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: Portfolio
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('../views/Projects.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/About.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/Contact.vue')
+    },
+    {
+      path: '/equestrian',
+      name: 'equestrian',
+      component: () => import('../views/Equestrian.vue')
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: () => import('../views/Blog.vue')
+    },
+    {
+      path: '/cv',
+      name: 'cv',
+      component: () => import('../views/CV.vue')
+    },
+    {
+      path: '/services',
+      name: 'services',
+      component: () => import('../views/Services.vue')
+    }
+  ],
 })
 
 export default router
